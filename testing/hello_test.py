7a585bb857914ec46aaa0b080c7361f06962fcf1
robbie <robbielcampbell@hotmail.com>
2023-03-26 12:40:14 +0100

    ⚗️ tests

--- testing/hello_test.py
+++ testing/hello_test.py
@@ -2,5 +2,7 @@ import unittest
 from messages.messages_no_args.hello import Hello
 
 class TestHelloMessage(unittest.TestCase):
-    def test_response(self):
+    """Tests the hello message package.
+    """
+    def test_response(self) -> None:
         self.assertEqual("Hello there mate!", Hello().message_to_send())
